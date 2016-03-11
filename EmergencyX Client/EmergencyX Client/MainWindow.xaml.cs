2b59a722a8879128e82b63340fbd7326e4fca040
ciajoe <yannick.feilhauer@outlook.de>
2016-03-11 21:05:35 +0100

    Progam is now able to list installed mods; Corrected a typo in german localisation

--- EmergencyX Client/EmergencyX Client/MainWindow.xaml.cs
+++ EmergencyX Client/EmergencyX Client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace EmergencyX_Client
 {
@@ -22,30 +23,51 @@ namespace EmergencyX_Client
     {
 		public MainWindow()
 		{
-			InitializeComponent();
-
-			//if Emergency 5 is installed and no mods are installed the text "Emergency is installed" should be displayed
-			//if its insatlled and there are mods the mods should be displayed 
-			//otherwise there should be the text "Emergency is not installed"
-			//
-
-			EmergencyInstallation myEmergencyInstallation = new EmergencyInstallation();
-
-			if(EmergencyInstallation.getIsEmergencyInstalled() && myEmergencyInstallation.verifyEmergencyInstallation(myEmergencyInstallation.getEmergencyInstallationPath())) 
-			{
-				lblIsEmergencyInstalled.Content = Properties.Resources.emergencyIsInstalled;
-			}
-			else 
-			{
-				lblIsEmergencyInstalled.Content = Properties.Resources.emergencyIsNotInstalled;
-			}
-			
+			InitializeComponent();			
 		}
 
 		private void settingsClick(object sender, RoutedEventArgs e)
 		{
 			Settings emergencyXSettings = new Settings();
 			emergencyXSettings.Show();
+		}
+
+		private void mainWindowLoaded(object sender, RoutedEventArgs e)
+		{
+
+			//if Emergency 5 is installed and no mods are installed the text "Emergency is installed" should be displayed
+			//if its insatlled and there are mods the mods should be displayed 
+			//otherwise there should be the text "Emergency is not installed"
+			//
+			EmergencyInstallation myEmergencyInstallation = new EmergencyInstallation();
+			string appDataModificationsJsonFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Promotion Software GmbH\EMERGENCY 5\mods\mods_user_settings.json";
+			if(File.Exists(appDataModificationsJsonFile))
+			{ 
+				MessageBox.Show(appDataModificationsJsonFile);
+			}
+
+			if (EmergencyInstallation.getIsEmergencyInstalled() && myEmergencyInstallation.verifyEmergencyInstallation(myEmergencyInstallation.getEmergencyInstallationPath()))
+			{
+				ModTools modTools = new ModTools(appDataModificationsJsonFile);
+				liModListBox.Visibility = Visibility.Visible;
+
+				Dictionary<int, string> modifications = modTools.getModifications();
+
+				foreach (var key in modifications)
+				{
+					ListBoxItem item = new ListBoxItem();
+					item.Content = key.Value;
+					liModListBox.Items.Add(item);
+				}
+
+			}
+			else
+			{
+				lblIsEmergencyInstalled.Content = Properties.Resources.emergencyIsNotInstalled;
+				liModListBox.IsEnabled = false;
+				liModListBox.Visibility = Visibility.Hidden;
+
+			}
 		}
 	}
 }
---
+++ EmergencyX Client/EmergencyX Client/ModTools.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace EmergencyX_Client
+{
+/// <summary>
+/// A collection of useful methods related to mods
+/// </summary>
+	public class ModTools
+	{
+		private Dictionary<int, string> modifications;
+		//Dictionary<string, string> options;
+		
+		public Dictionary<int,string> getModifications()
+		{
+			return this.modifications;
+		}
+
+		public void setModifications(string jsonFilePath)
+		{
+			this.modifications = new Dictionary<int, string>();
+			
+			//first read all "top-level" key which are the mod names
+			//each of them contains as value is options eg. "Enabled" or OderingIndex also in JSON Noation
+			Dictionary<string, object> modNames = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(jsonFilePath));
+
+			//go through all mods
+			int counter = 0;
+			
+
+			foreach (var mod in modNames)
+			{
+				this.modifications.Add(counter, mod.Key);
+
+				/* **** The follwing code is uncommented because it is currently not needed, but could maybe helpfull as example ****
+				 * 
+				Dictionary<string, object> modOptions = JsonConvert.DeserializeObject<Dictionary<string, object>>(mod.Value.ToString());
+
+				//go also through all options
+				int count = 0;
+				foreach (var opt in modOptions) 
+				{
+					this.options.Add(mod.Key + count.ToString(),)
+				}
+				*/
+
+				counter++;
+			} //end foreache
+			
+		} //end setModifications;
+		
+		public ModTools(string jsonFile)
+		{
+			setModifications(jsonFile);
+		}
+
+	}
+}
