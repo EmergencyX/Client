fd61e90d3a67ba098275d958436fcbbdf30caaf8
ciajoe <yannick.feilhauer@outlook.de>
2016-03-17 00:19:26 +0100

    Did many stuff; our Client shows know the installed mods and there options (activity status and ordering index)

--- EmergencyX Client/EmergencyX Client/MainWindow.xaml.cs
+++ EmergencyX Client/EmergencyX Client/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using EmergencyX.Emergency5.Modifications;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,13 @@ namespace EmergencyX_Client
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		
+		public List<InstalledMod> InstalledMods { get; set; }
+		
 		public MainWindow()
 		{
 			InitializeComponent();
+
 		}
 
 		private void settingsClick(object sender, RoutedEventArgs e)
@@ -32,40 +37,54 @@ namespace EmergencyX_Client
 			emergencyXSettings.Show();
 		}
 
+		//some functions in here work better if Loaded Event is used.. Dont know why...
 		private void mainWindowLoaded(object sender, RoutedEventArgs e)
 		{
+			updateSpecificWindowData();
+		}
+
+		private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		#region updateSpecificWindowData
+		private void updateSpecificWindowData()
+		{
+			//Read Text from description in AssemblyInfo.cs and display it while app is running
+			Version.Text = ((System.Reflection.AssemblyDescriptionAttribute)System.Reflection.Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(System.Reflection.AssemblyDescriptionAttribute), false)[0]).Description;
 
 			//if Emergency 5 is installed and no mods are installed the text "Emergency is installed" should be displayed
 			//if its insatlled and there are mods the mods should be displayed 
 			//otherwise there should be the text "Emergency is not installed"
 			//
+
 			EmergencyInstallation myEmergencyInstallation = new EmergencyInstallation();
 
-			///holds the full path to appdata mod 
+			//holds the full path to appdata mod 
 			string appDataModificationsJsonFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Promotion Software GmbH\EMERGENCY 5\mods\mods_user_settings.json";
 
+			//if somewhere (somehow) an emergency installation is found and this installation could been verified
 			if (EmergencyInstallation.getIsEmergencyInstalled() && myEmergencyInstallation.verifyEmergencyInstallation(myEmergencyInstallation.getEmergencyInstallationPath()))
 			{
-				ModTools modTools = new ModTools(appDataModificationsJsonFile);
-				liModList.Visibility = Visibility.Visible;
+				ModTools mainWindowModTools = new ModTools(appDataModificationsJsonFile);
+				InstalledMods = mainWindowModTools.getInstalledModifications();
 
-				Dictionary<int, string> modifications = modTools.getModifications();
-
-				foreach (var key in modifications)
-				{
-					ListBoxItem item = new ListBoxItem();
-					item.Content = key.Value;
-					liModList.Items.Add(item);
-				}
+				DataContext = this;
 
 			}
 			else
 			{
+				// if not display the text "Emergency is not installed" and dis able the list
 				lblIsEmergencyInstalled.Content = Properties.Resources.emergencyIsNotInstalled;
-				liModList.IsEnabled = false;
-				liModList.Visibility = Visibility.Hidden;
 
 			}
+		}
+		#endregion updateSpecificWindowData
+
+		private void VisibilityChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			updateSpecificWindowData();
 		}
 	}
 }
---
+++ EmergencyX Client/EmergencyX.Emergency5.Modifications/InstalledMods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmergencyX.Emergency5.Modifications
+{
+	public class InstalledMod
+	{
+		public string modificationName { get; set; }
+		public string modificationActivationStatus { get; set; }
+		public string modificationOrderingIndex { get; set; }
+	}
+}
