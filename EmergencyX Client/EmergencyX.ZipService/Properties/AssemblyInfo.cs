e64191693a823c70c8b15a8a84a87196811c2ae5
ciajoe <yannick.feilhauer@outlook.de>
2016-04-03 23:02:56 +0200

    Starting Zip-Development

---
+++ EmergencyX Client/EmergencyX.ZipService/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("EmergencyX.ZipService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmergencyX.ZipService")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("2c0dceb1-08fa-4a73-9cd3-07c4cc5e2424")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EmergencyX Client/EmergencyX.ZipService/Unzipper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace EmergencyX.ZipService
+{
+	/// <summary>
+	/// Contains all the things related with unpacking a File
+	/// </summary>
+	public class Unzipper
+	{
+
+		private string zipFileName;
+		
+		public string getZipFileName()
+		{
+			return this.zipFileName;
+		}
+
+		public void setZipFileName(string zipFileName)
+		{
+			this.zipFileName = zipFileName;
+		}
+
+		public void unpackModZip(string targetPath)
+		{
+			
+			try { 
+				ZipArchive myArchiv = ZipFile.Open(this.zipFileName, ZipArchiveMode.Read);
+				myArchiv.ExtractToDirectory(targetPath);
+				myArchiv.Dispose();
+			} 
+			catch (Exception e)
+			{
+				
+			}
+
+			try
+			{
+				//File
+			}
+			catch (Exception ex)
+			{
+
+			}
+
+		}
+	
+	}
+}
